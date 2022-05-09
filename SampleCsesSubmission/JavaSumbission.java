f5f400bb62c543c93bf368189d1fb4e0260c0eca
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-05-09 15:06:08 +0530

    Added sample cses submissions for java and c++

---
+++ SampleCsesSubmission/JavaSumbission.java
@@ -0,0 +1,29 @@
+package SampleCsesSubmission;
+
+
+// CSES uses OpenJDK 1.8.0_91 (Java 8).
+
+// Name your code file according to the java rules. 
+// The file containing class called Main should be upload as Main.java. 
+// The code must not be in any packages or the compilation will fail.
+
+// Java's default Scanner and System.out.println may be too slow for input and output. 
+// The runtime environment contains a faster class for handling input and output called IO. 
+// It doesn't need to be imported as it resides in the same directory as the program. 
+// You can use IO class as follows:
+
+public class JavaSumbission {
+    public static void main(String[] args) {
+        IO io = new IO();
+        
+        String a = io.next(); // Reads the next string separated by spaces.
+        int b = io.nextInt(); // Reads the next int separated by spaces.
+        long c = io.nextLong(); // Reads the next long separated by spaces.
+        double d = io.nextDouble(); // Reads the next double separated by spaces.
+        
+        // Works like System.out.println.
+        io.println("Input was " + a + " " + b + " " + c + " " + d);
+        
+        io.close(); // MUST BE CALLED IN THE END, otherwise some of the output may be missing
+    }
+    }
---
+++ SampleCsesSubmission/cppSubmission.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// The C++ compiler used by CSES is g++ 4.8.4. Used flags are -std=c++0x -O2 -Wall.
+
+// The default cin and cout may be too slow as default 
+// but they can be made much faster by removing synchronization with C's stdio library. 
+// This can be done by adding line
+// cin.sync_with_stdio(false);
+// in the begin of main function:
+
+int main() {
+    cin.sync_with_stdio(false);
+    
+    // solve the task here
+}

878724bd7c3eae498e824bc07c087b48c129b481
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-05-10 17:32:43 +0530

    Added weird algorithm accepted solution

--- IntroductoryProblems/weird_algorithm/Main.java
+++ IntroductoryProblems/weird_algorithm/Main.java
@@ -1,7 +1,28 @@
 package IntroductoryProblems.weird_algorithm;
 
+//ACCEPTED
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        List<Long> result = new ArrayList<Long>();
+
+        long n = sc.nextLong();
+        result.add((long)n);
+
+        while (n != 1) {
+            if (n%2 == 0) {
+                n = n/2;
+                result.add(n);
+            } else {
+                n = n*3+1;
+                result.add(n);
+            }
+        }
+        result.forEach(e -> System.out.print(e+" "));
     }
 }

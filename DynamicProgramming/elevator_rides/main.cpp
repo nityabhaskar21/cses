7236ca25032a3bd34951605a7666a06cd9ceeff8
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-06-20 17:10:10 +0530

    elevator rides commit 2

--- DynamicProgramming/elevator_rides/main.cpp
+++ DynamicProgramming/elevator_rides/main.cpp
@@ -62,5 +62,32 @@ int main()
     //    freopen("output.txt", "w", stdout);
     // #endif
 
+    int n;
+    long x;
+    cin>>n>>x;
+
+    v64 weights(n, 0);
+
+    for (int i = 0; i <n; i++) {
+        cin>>weights[i];
+    }
+
+    vector<vector<int>> t(n+1, vector<int>(x+1, 0));
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            if (i== 0 || j == 0) {
+                t[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+           
+        }
+    }
+
+
     return 0;
 }
